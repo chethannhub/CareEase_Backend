728aa036ee87b382a1f6689537bf8b58f9b330f0
C-hetha-N <chethannchethann80@gmail.com>
2025-02-02 15:57:50 +0530

    getpatient data using id, email and password was done

--- models/patient.js
+++ models/patient.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const patientSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    phone: { type: String, required: true },
+    name: { type: String, required: false },
+    age: { type: Number, required: false },
+    phone: { type: String, required: false },
     email: { type: String, required: false },
-    gender: { type: String, required: true },
-    address: { type: String, required: true },
-    problem: { type: String, required: true },
+    password: { type: String, required: false },
+    gender: { type: String, required: false },
+    address: { type: String, required: false },
+    problem: { type: String, required: false },
     habits: {
         smokes: { type: Boolean, default: false },
         exercises: { type: Boolean, default: false },
