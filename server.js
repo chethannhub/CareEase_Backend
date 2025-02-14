042fbf2981b65e6f0404d4077e0892b366354e5b
C-hetha-N <chethannchethann80@gmail.com>
2025-02-14 14:29:54 +0530

    setuping for deploying in vercel

--- server.js
+++ server.js
@@ -30,4 +30,11 @@ app.use("/api/beds", bedsRoutes);
 app.use("/api/bills", billsRoutes);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+// if (process.env.NODE_ENV !== "production") {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://localhost:${PORT}`);
+//     });
+//   }
+
+export default app;
